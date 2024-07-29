aaf0020cda4bd4fa79a414bccd315414c23ddae6
rashid husianat <rashidjihad9@gmail.com>
2024-07-29 20:25:05 +0300

    first commit

---
+++ Sevices/Catalog/EShop.CatalogAPI/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,19 @@
+﻿using Carter;
+using MediatR;
+using OpenTelemetry.Trace;
+
+namespace EShop.CatalogAPI.Products.CreateProduct;
+
+    public class CreateProductEndpoint : ICarterModule
+    {
+        public void  AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("products", async (ISender sender,CreateProductCommand command) =>
+            {
+                var result =await sender.Send(command);
+
+                return Results.Ok();
+            });
+        }
+    }
+
---
+++ Sevices/Catalog/EShop.CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,36 @@
+﻿using EShop.CatalogAPI.Model;
+using Mapster;
+using Marten;
+using MediatR;
+
+namespace EShop.CatalogAPI.Products.CreateProduct;
+
+public class CreateProductCommand:IRequest<CreateProductResult>
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = default!;
+    public List<string> Category { get; set; } = new();
+    public string Description { get; set; } = default!;
+    public string ImageFile { get; set; } = default!;
+    public decimal Price { get; set; }
+
+}
+
+public class CreateProductResult
+{
+    public Product Product { get; set; }
+}
+
+public class CreateProductHandler(IDocumentSession session)
+    : IRequestHandler<CreateProductCommand, CreateProductResult>
+{
+   
+    public async Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        var product=request.Adapt<Product>();
+        session.Store<Product>(product);
+        await session.SaveChangesAsync(cancellationToken);
+        return product.Adapt<CreateProductResult>();
+    }
+}
+
